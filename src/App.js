14485dd72d30b69a20333ff5e5f8ff7706671154
jiwon <zest1923@gmail.com>
2021-11-08 13:15:26 +0900

    deps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Button from "./Button"; 
 import styles from "./App.module.css";
+import {useState, useEffect} from "react"
 
 function App() {
+  const [count, setCount] = useState(0);
+  const onClick = () => setCount((prev) => prev + 1);
+  //const iRunOnlyOnce = () => console.log("I run only once");
+  console.log("I run all the time");
+  //useEffect(iRunOnlyOnce, []); // 단 한번만 실행될 수 있도록 도와주는 함수. (API 호출 등에 사용)
+  
+  useEffect(()=> console.log("Call the API..."), []) // 위와 동일한 코드
   return (
     <div>
-      <h1 className ={styles.title}>Welcomeback React!</h1>
-      <Button text="continue"/>
+      <h1 > {count} </h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
