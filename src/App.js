088e3ae1feea7b9b1b5dc953923a9129327d0e68
jiwon <zest1923@gmail.com>
2021-11-09 17:11:37 +0900

    movie app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
 import React from "react";
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react"
 
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [loading, setLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+  const getMovies = async () => { //then 대신 
+    // const response = await fetch(
+    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    //   );
+    //const json = await response.json();
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  console.log(movies)
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo == '') return; // todo가 비어있다면 함수 작동 x
-    setToDos((currentArray => [toDo, ...currentArray])) // 기존 값과 새로운 값을 리스트로 연결
-    setToDo(""); // 비어있지 않은 상태에서 submit 하면 input 초기화
-  };
-
+  useEffect(() => {
+    getMovies();
+    // fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+    //   .then((respone) => respone.json)
+    //   .then((json) => {
+    //     setMovies(json.data.movies)
+    //     setLoading(false);
+    //   })
+  }, [])
   return (
     <div>
-      <h1> MY TODO ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your todo..." />
-        <button>Add To Do </button>
-      </form>
-      <ul>
-        {toDos.map((item, index) =>
-          <li key = {index}>{ item}</li>)}
-      </ul>
+      {loading ? <h1>Loading...</h1> :
+        <div>{
+          movies.map(movie =>
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) =>
+                  (<li key = {g}> {g} </li>))
+                }
+              </ul>
+            </div>)}
+        </div>}
+
     </div>
   )
 }
